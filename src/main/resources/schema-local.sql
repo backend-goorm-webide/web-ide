8ce39855061eeaca09d61bd863c5bd3fa0f062b7
Timothy <timothy@jungnanum.com>
2023-12-12 18:07:21 +0900

    chore(#2): 각각의 개발환경 설정
    - 로컬 개발에 사용되는 H2 인 메모리 테스트용 쿼리.
    - DB에 테이블이 생성이 되는지 확인하기 위한 용도.

---
+++ src/main/resources/schema-local.sql
@@ -0,0 +1,7 @@
+-- H2
+CREATE TABLE IF NOT EXISTS `example_table` (
+                                               `id` INT NOT NULL AUTO_INCREMENT,
+                                               `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
